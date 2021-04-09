814328cc651129fadf90599843dc579da6fd1539
raniamarhoni <marhoni@outlook.com>
2021-04-09 20:13:12 +0000

    removed secret key and set debug

--- sheysskinkitchen/settings.py
+++ sheysskinkitchen/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8@soti55#zmic=11sj=7pt7iua_e#s5n#y4711g#^2y#@d%cv6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sheyskinkitchen.herokuapp.com', 'localhost']
 
