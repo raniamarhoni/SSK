de4568af31203c8962aace58241e60f3445df46d
raniamarhoni <marhoni@outlook.com>
2021-04-11 19:16:31 +0000

    allowed hosts order changed

--- sheysskinkitchen/settings.py
+++ sheysskinkitchen/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['sheyskinkitchen.herokuapp.com', 'localhost']
-
+ALLOWED_HOSTS = ['localhost', 'sheyskinkitchen.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
