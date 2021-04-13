a2b3f64c66300a01c52f13e71041e14635516601
raniamarhoni <marhoni@outlook.com>
2021-04-13 17:31:26 +0000

    product detail page added and links added

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Product, Category, Size
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+from .models import Product, Category
 
 
 # Create your views here.
@@ -55,3 +58,14 @@ def all_products(request):
 
     return render(request, 'products/products.html', context)
 
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
