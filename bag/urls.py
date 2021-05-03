a380b5a57d63734c6464fcb68bd2fa30acdb2056
raniamarhoni <marhoni@outlook.com>
2021-05-03 01:36:31 +0000

    added add to bag functionality

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
+    path('add/<int:product_id>/<int:size_id>/',
+         views.add_to_bag, name='add_to_bag'),
 ]
